031ae553a8943e1b08d5a9d1b89f8162ed1c0afb
Robin Jones <RobinJones335@gmail.com>
2020-03-25 14:00:31 -0400

    testing push to heroku

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan")
 const app = express();
-const path = require('path'); 
+const path = require('path');
+
 
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 
-
+require('dotenv').config()
 
 db = mongoose.connection;
 
