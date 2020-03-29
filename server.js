5fb23db41ec3fa2ee34f8bf0a4168e42a3916ce5
Robin Jones <RobinJones335@gmail.com>
2020-03-28 23:27:03 -0400

    removed the ( from process env

--- server.js
+++ server.js
@@ -1,39 +1,40 @@
+// Require and instantiate express
 const express = require("express");
-const mongoose = require("mongoose");
-const logger = require("morgan")
+const logger = require("morgan");
 const app = express();
-const path = require('path');
+const path = require("path");
+const mongoose = require("mongoose");
 
+// Assign port
+const PORT = process.env.PORT || 3500;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 
-const PORT = process.env.PORT || 3000;
-
-// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-
-// const MONGODB_URI = 'mongodb+srv://Robin:juanita1@workoutplan-cxiwb.mongodb.net/test?retryWrites=true&w=majority'
-// const mongodb = process.env.MONGODB_URI || "mongodb://localhost/workout";
 db = mongoose.connection;
 
+
 db.once('open', () => {
-  console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB');
 })
 
+//check for any db errors
 db.on('error', (err) => {
-  console.log(err)
+    console.log(err)
 });
 
 
+//Set up middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 app.use(logger("dev"));
 
-app.use(express.static("public"));
+// Requiring routes
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/workout', {useNewUrlParser:true});
+mongoose.connect(MONGODB_URI, {useNewUrlParser:true});
 
-// routes
-require("./routes/api-routes")(app);
-require("./routes/html-route") (app);
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`Server is listening on Port ${PORT}!`);
 });
